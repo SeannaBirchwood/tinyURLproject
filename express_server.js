a4ec8f187495d79903a59e55b654b1ea1917c3a1
Chelsea Craig <chelsea.craig1223@gmail.com>
2016-10-11 22:47:23 +0000

    attempting to print shortURLs. So far without success

--- express_server.js
+++ express_server.js
@@ -1,3 +1,4 @@
+"Use Strict"
 //express_server.js
 //load the things we need
 
@@ -7,6 +8,10 @@ var app = express();
 //set the view engine to ejs
 
 app.set("view engine", "ejs");
+
+//this relates to the body-parser package we installed
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded());
 
 //use res.render to load up an ejs view file
 
@@ -36,7 +41,7 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
-app.get("/urls.json", (req, res) => {
+app.get("/urls", (req, res) => {
   res.json(urlDatabase);
 });
 
@@ -44,7 +49,50 @@ app.get("/hello", (req, res) => {
   res.end("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
 
+app.get("/urls/:id", (req, res) => {
+  let templateVars = { shortURL: req.params.id };
+  res.render("urls_show", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+	res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+	console.log(req.body); //debug statement to see POST parameters
+	res.send("OK"); //respond with 'OK' (we will replace this)
+});
+
+//instructions saywe receive POST request "/urls/create"
+//which whould generate our shortURL? we can add the new
+//pairs to our database?
+//so I added this code, and put the previous POST info
+//inside it, b/c I don't understand.
+app.post("/urls/create", (req, res) => {
+	console.log(req.body);
+	res.send("OK")
+})
+
+//placeholder function for something in the next excercise
+function generateRandomString() {
+	console.log("I am here. But where is here?")
+}
+console.log(generateRandomString());
+
+
+
+app.get("/u/:shortURL", (req, res) => {
+	let longURL = urlDatabase.keys[0];
+	console.log(longURL)
+	res.redirect(longURL); 
+})
+
+shortURL
 
 
 //eventual problem
