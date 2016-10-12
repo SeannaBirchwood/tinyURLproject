c89cd1b2b741c9cc826f2e700bd074a25d8d4318
Chelsea Craig <chelsea.craig1223@gmail.com>
2016-10-12 22:21:36 +0000

    Changed into more ES6 terms, got randomString to work

--- express_server.js
+++ express_server.js
@@ -2,9 +2,9 @@
 //express_server.js
 //load the things we need
 
-var express = require("express");
-var app = express();
-var PORT = process.env.PORT || 8080; // default port 8080
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8080; // default port 8080
 
 //set the view engine to ejs
 
@@ -12,31 +12,32 @@ app.set("view engine", "ejs");
 
 //this relates to the body-parser package we installed
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
-
+app.use(bodyParser.urlencoded({extended: true})); //test out {extended: false} later
+const morgan = require("morgan")
+app.use(morgan('dev'));
 //use res.render to load up an ejs view file
 
 //index page
 
 app.get('/', function(req, res) {
-    res.render('pages/index');
+  res.render('pages/index');
 });
 
 // about page 
 app.get('/about', function(req, res) {
-    res.render('pages/about');
+  res.render('pages/about');
 });
 
 
 
-var urlDatabase = {
+let urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", function(req, res) {
-  res.send("Hello!");
-});
+// app.get("/", function(req, res) {
+//   res.send("Hello!");
+// });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
@@ -65,11 +66,12 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
+
 	console.log(req.body); //debug statement to see POST parameters
 	res.send("OK"); //respond with 'OK' (we will replace this)
 });
 
-//instructions saywe receive POST request "/urls/create"
+//instructions say we receive POST request "/urls/create"
 //which whould generate our shortURL? we can add the new
 //pairs to our database?
 //so I added this code, and put the previous POST info
@@ -79,22 +81,33 @@ app.post("/urls/create", (req, res) => {
 	res.send("OK")
 })
 
-//placeholder function for something in the next excercise
-// function generateRandomString() {
-// 	console.log("I am here. But where is here?")
-// }
-// console.log(generateRandomString());
+function generateRandomString() {
+  let text = "";
+  let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for(let i = 0; i < 6; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+  return text;
+}
 
 
+//req.body.longUrl
 
 app.get("/u/:shortURL", (req, res) => {
-  //pseudo: let longURL equal urlDatabase.key.obj if req.param.id equals urlDatabase.key
-	let longURL = urlDatabase["key"]["object"] {
-    if (req.param.id == urlDatabase.key);
-  } req.param.id; //if we want the input to redirect to longURL,
-	res.redirect(302, "/urls:id", longURL); //we shouldn't make the longURL redirect
-                                          //to the input...
-});
+                                             //pseudo: let longURL equal urlDatabase.key.obj
+  for (let i = 0; i < urlDatabase.length; i++) {
+	let longURL = urlDatabase[i][0]          //indexOf(urlDatabase)
+  console.log(longURL);
+  }                                           //if req.param.id equals urlDatabase.key
+  //if (req.param.id == urlDatabase) {
+                                             //if we want the input to redirect to longURL,
+  //res.redirect(301, longURL);
+});                                        //we shouldn't make the longURL redirect to the input...
+
+
+//curl -Li http://localhost:8080/u/b2xVn2
+
 
 
 
