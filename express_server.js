9282d87e23a7eafa002df10cef26c0f4c5043336
Chelsea Craig <chelsea.craig1223@gmail.com>
2016-10-13 20:17:04 +0000

    Cleaned up some unnecessary pages

--- express_server.js
+++ express_server.js
@@ -1,59 +1,57 @@
 "Use Strict"
-//express_server.js
-//load the things we need
 
+/********************************Page Setup**************************************/ 
+
+const PORT = process.env.PORT || 8080;
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 8080; // default port 8080
-
-//set the view engine to ejs
-
-app.set("view engine", "ejs");
-
-//this relates to the body-parser package we installed
+const morgan = require("morgan");
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true})); //test out {extended: false} later
-const morgan = require("morgan")
+const textArgs = process.argv.splice(2);
+
 app.use(morgan('dev'));
-//use res.render to load up an ejs view file
-
-//index page
-
-app.get('/', function(req, res) {
-  res.render('pages/index');
-});
-
-// about page 
-app.get('/about', function(req, res) {
-  res.render('pages/about');
-});
-
-
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
 
 let urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-// app.get("/", function(req, res) {
-//   res.send("Hello!");
-// });
-
+//this is just to make sure that the port is actually "listening".
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
-app.get("/urls", (req, res) => {
-  res.json(urlDatabase);
-});
+/********************************************************************************/
 
-app.get("/hello", (req, res) => {
-  res.end("<html><body>Hello <b>World</b></body></html>\n");
-});
+/*********************************Notes******************************************/
 
-app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
+//get is the handler for requests by the browser
+//post is the handler for info from the browser
+//The default port is 8080
+//curl -Li http://localhost:8080/u/b2xVn2
+
+/********************************************************************************/
+
+/**********************************Get Requests**********************************/
+
+//  app.get('/', function(req, res) {
+//    res.render('urls_index');
+// });
+
+app.get('/about', function(req, res) {
+  let urlsIndex = urlDatabase
+  res.render('urls_show', urlsIndex);
+ });
+
+// app.get("/hello", (req, res) => {
+//   res.end("<html><body>Hello <b>World</b></body></html>\n");   //Why is this here??? There's no corresponding page.
+// });
+
+app.get('/urls', (req, res) => {
+  let templateVars = urlDatabase
+  res.render('urls_index', templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
@@ -61,35 +59,53 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
-app.get("/urls/new", (req, res) => {
-	res.render("urls_new");
+app.get("/new", (req, res) => {
+  let newShortUrl = generateRandomString(textArgs);
+	res.render("urls_new", newShortUrl);
+  urlDatabase.generateRandomString(req) = "req";
 });
 
-app.post("/urls", (req, res) => {
-
-	console.log(req.body); //debug statement to see POST parameters
-	res.send("OK"); //respond with 'OK' (we will replace this)
+app.get("/u/:shortURL", (req, res) => {
+  let longURL = Object.keys(urlDatabase).forEach(function (key) {
+    console.log(data[key]);
+  })
+  res.redirect(longURL);
 });
 
-//instructions say we receive POST request "/urls/create"
-//which whould generate our shortURL? we can add the new
-//pairs to our database?
-//so I added this code, and put the previous POST info
-//inside it, b/c I don't understand.
-app.post("/urls/create", (req, res) => {
-	console.log(req.body);
-	res.send("OK")
-})
+/*******************************************************************************/
 
-function generateRandomString() {
+/***********************************Post Requests*******************************/
+
+app.post("/new", (req, res) => {
+  let newShortUrl = generateRandomString(textArgs);
+  //newShortUrl.generateRandomString()
+  res.redirect("urls_show");
+  console.log(req);
+  console.log(newShortUrl);
+});
+
+// app.post("/urls/create", (req, res) => {
+// 	console.log(req.body);
+// 	res.send("OK")
+//   res.redirect("urls_new");
+// })
+
+/******************************************************************************/
+
+/**********************************Functions***********************************/
+
+function generateRandomString(textArgs) {
   let text = "";
   let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-  for(let i = 0; i < 6; i++)
+  for(let i = 0; i < 6; i++) {
     text += possible.charAt(Math.floor(Math.random() * possible.length));
-
-  return text;
+  }
+  return text
+  
 }
+console.log(generateRandomString());
+
 
 
 //req.body.longUrl
@@ -97,7 +113,7 @@ function generateRandomString() {
 app.get("/u/:shortURL", (req, res) => {
                                              //pseudo: let longURL equal urlDatabase.key.obj
   for (let i = 0; i < urlDatabase.length; i++) {
-	let longURL = urlDatabase[i][0]          //indexOf(urlDatabase)
+	let longURL = urlDatabase[i][0];          //indexOf(urlDatabase)
   console.log(longURL);
   }                                           //if req.param.id equals urlDatabase.key
   //if (req.param.id == urlDatabase) {
@@ -105,38 +121,3 @@ app.get("/u/:shortURL", (req, res) => {
   //res.redirect(301, longURL);
 });                                        //we shouldn't make the longURL redirect to the input...
 
-
-//curl -Li http://localhost:8080/u/b2xVn2
-
-
-
-
-
-//shortURL
-
-
-//eventual problem
-// <!DOCTYPE html>
-// <html lang="en">
-// <body>
-//     <!-- Write EJS code here to display shortened links -->
-// </body>
-// </html>
-
-// router.get('/:id', function(req, res) {
-//   var apple = appleDB.filter(function(apple){
-//     return (apple.id == req.params.id);
-//   })[0];
-
-//   if (apple) {
-//     res.json(apple);
-//   }
-//   else {
-//     // Since we couldn't find what we were looking for,
-//     // we should stick to HTTP conventions and reply with
-//     // status 404 (not found).
-//     res.status(404).json({
-//       error: `Apple id=${req.params.id} not found`
-//     });
-//   }
-// })
