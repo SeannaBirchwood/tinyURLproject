1f267dcdf120d722119415f51f59c5808bcb6a9e
Chelsea Craig <chelsea.craig1223@gmail.com>
2016-11-02 18:18:09 +0000

    Have url object almost working

--- express_server.js
+++ express_server.js
@@ -1,5 +1,28 @@
 "Use Strict"
 
+/*
+2. when creating a link, associate the link with the user (you may need to 
+    update the data structure for how links are stored)
+    - this should create an object inside an object owned by
+      user_id
+    - I should do this in the server, probably in the POST request
+      that is handling generateRandomString
+
+**4. only the user that created a link can edit or delete the link
+    - if user_id matches user_id.shortUrl, show user edit, delete
+    buttons
+    - This should be on urls_index
+    ** - might be done
+
+5. make sure that anyone can still visit the shortlinks and get properly redirected
+    - show all visitors to page only urls, urls_home / urls_index
+
+7. There's a weird error happening that says "can't set headers
+    after they are sent", but nothing else seems to break with it.
+    Fix later.
+
+*/
+
 /********************************Page Setup**************************************/ 
 
 const PORT = process.env.PORT || 8080;
@@ -24,13 +47,27 @@ app.listen(PORT, () => {
 /*********************************Databases**************************************/
 
 let urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
-};
+  "00000001": {
+    shortURL: "b2xVn2",
+    longURL: "http://www.lighthouselabs.ca"
+  },
+  "00000002": {
+    shortURL: "9sm5xK",
+    longURL: "http://www.google.com"
+  }
+}
 
 let globalUsers = {
-  "userRandomID": {id: "userRandomID", email: "user@example.com", password: "purple-monkey-dinosaur"},
-  "user2RandomID": {id: "user2RandomID", email: "user2@example.com", password: "dishwasher-funk"}
+
+  "userRandomID": 
+    {id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"},
+
+  "user2RandomID": 
+    {id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"}
 }
 
 /********************************************************************************/
@@ -47,40 +84,41 @@ let globalUsers = {
 /**********************************Get Requests**********************************/
 
 app.get('/', (req, res) => {
-  let urlsIndex = {
-    urls: urlDatabase,
-    username: req.cookies["username"]
-  };
-  res.render('urls_home', urlsIndex);
+  let userURLs = urlDatabase;
+  let userID = req.cookies['user_id'];
+  let templateVars = {userURLs:userURLs,user_id:userID}
+  console.log("Show me urls!", userURLs)
+  res.render('urls_home', templateVars);
 });
 
+
 app.get('/urls', (req, res) => {
-  let urlsIndex = {
-    urls: urlDatabase,
-    username: req.cookies["username"]
-  };
-  res.render('urls_index', urlsIndex);
+  let userURLs = urlDatabase[req.cookies["user_id"]]
+  let userID = globalUsers[req.cookies["user_id"]];
+  let templateVars = {userURLs:userURLs,user_id:userID}
+  res.render('urls_index', templateVars);
 })
 
 app.get("/urls/:id", (req, res) => {
   let shortURL = {
     shortURL: req.params.id,
-    username: req.cookies["username"]
+    user_id: req.cookies["user_id"]
   };
   res.render("urls_show", shortURL);
 });
 
 app.get("/new", (req, res) => {
-  let newShortUrl = {
-    username: req.cookies["username"]
+  let newUrl = {
+    urlDatabase: uid,
+    user_id: req.cookies["user_id"]
   };
-  res.render("urls_new", newShortUrl);
+  res.render("urls_new", newUrl);
 });
 
 app.get("/u/:shortURL", (req, res) => {
   let longURL = {
     shortURL: req.params.id,
-    username: req.cookies["username"]
+    user_id: req.cookies["user_id"]
   };
   res.redirect(longURL);
 });
@@ -88,26 +126,46 @@ app.get("/u/:shortURL", (req, res) => {
 app.get("/register", (req, res) => {
   let templateVars = {
     shortURL: req.params.id,
-    username: req.cookies["username"],
+    user_id: req.cookies["user_id"]
   };
+  console.log(templateVars, "Show me user object")
   res.render("urls_register", templateVars)
 });
 
-// app.get("/login", (req, res) => {
-
-// });
+app.get("/login", (req, res) => {
+  let templateVars = {
+    shortURL: req.params.id,
+    user_id: req.cookies["user_id"]
+  };
+  res.render("urls_login", templateVars);
+ });
 
 /*******************************************************************************/
 
 /***********************************Post Requests*******************************/
 
 app.post("/urls", (req, res) => {
-  urlDatabase[generateRandomString()] = req.body["longURL"];
+  let url = {};
+  url.shortURL = randomURL;
+  url.longURL = req.body['longURL'];
+  urlDatabase[uid] = url;
+  console.log(urlDatabase, "This should be the url dict");
   res.redirect("/urls");
 });
 
+app.post("/register", (req, res) => {
+  let user = {'id': uid};
+  user.email = req.body["email"];
+  user.password = req.body["password"];
+  globalUsers[uid] = user;
+  res.redirect("/");
+});
+
 app.post("/urls/:id/delete", (req, res) => {
-  let templateVars = {shortURL: req.params.id};
+  let templateVars = {
+    shortURL: req.params.id,
+    user_id: req.cookies["user_id"]
+  };
   delete urlDatabase[templateVars.shortURL];
   res.redirect("/urls");
 });
@@ -120,15 +178,15 @@ app.post('/urls/:id/update', (req, res) => {
 });
 
 app.post("/login", (req, res) => {
-  console.log('post login')
+  //Object.keys creates an array of all the values so we can
+  //forEach, which we pass a new name for the array (userID).
   Object.keys(globalUsers).forEach( (userID) => {
     let userValue = globalUsers[userID];
     if(req.body.username === userValue.email && req.body.password === userValue.password) {
       res.cookie('user_id', userValue.id);
-      res.cookie('username', userValue.email);
       res.redirect('/');
     } else {
-      res.status(403).send(403, "You need to check yourself")
+      res.status(403).send("You need to check yourself")
     }
 
   })
@@ -137,19 +195,9 @@ app.post("/login", (req, res) => {
 
 app.post("/logout", (req, res) => {
   res.clearCookie('user_id');
-  res.clearCookie('username');
   res.redirect('/');
 });
 
-app.post("/register", (req, res) => {
-  let id = generateUserID();
-  let user = {'id': id};
-  user.email = req.body["email"];
-  user.password = req.body["password"];
-  globalUsers[id] = user;
-  console.log('in register',globalUsers)
-  res.redirect("/");
-});
 
 /******************************************************************************/
 
@@ -174,3 +222,6 @@ function generateUserID() {
   }
   return text
 }
+
+let uid = generateUserID();
+let randomURL = generateRandomString();
