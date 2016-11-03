75c1ca9ea8aaacb264cf4796a0b5609ca86bc931
Chelsea Craig <chelsea.craig1223@gmail.com>
2016-11-03 18:26:58 +0000

    user specific features working

--- express_server.js
+++ express_server.js
@@ -1,27 +1,5 @@
+
 "Use Strict"
-
-/*
-2. when creating a link, associate the link with the user (you may need to 
-    update the data structure for how links are stored)
-    - this should create an object inside an object owned by
-      user_id
-    - I should do this in the server, probably in the POST request
-      that is handling generateRandomString
-
-**4. only the user that created a link can edit or delete the link
-    - if user_id matches user_id.shortUrl, show user edit, delete
-    buttons
-    - This should be on urls_index
-    ** - might be done
-
-5. make sure that anyone can still visit the shortlinks and get properly redirected
-    - show all visitors to page only urls, urls_home / urls_index
-
-7. There's a weird error happening that says "can't set headers
-    after they are sent", but nothing else seems to break with it.
-    Fix later.
-
-*/
 
 /********************************Page Setup**************************************/ 
 
@@ -31,6 +9,9 @@ const app = express();
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
+const bcrypt = require('bcrypt');
+const password = globalUsers.user_id['password'];
+const hashed_password = bcrypt.hashSync(password, 10);
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: true}));
@@ -47,22 +28,22 @@ app.listen(PORT, () => {
 /*********************************Databases**************************************/
 
 let urlDatabase = {
-  "00000001": {
+  "00000001": [{
     shortURL: "b2xVn2",
     longURL: "http://www.lighthouselabs.ca"
   },
-  "00000002": {
+  {
     shortURL: "9sm5xK",
     longURL: "http://www.google.com"
-  }
+  }]
 }
 
 let globalUsers = {
 
-  "userRandomID": 
-    {id: "userRandomID", 
-    email: "user@example.com", 
-    password: "purple-monkey-dinosaur"},
+  "00000001": 
+    {id: "00000001", 
+    email: "hark@hark.com", 
+    password: "hark"},
 
   "user2RandomID": 
     {id: "user2RandomID", 
@@ -86,8 +67,8 @@ let globalUsers = {
 app.get('/', (req, res) => {
   let userURLs = urlDatabase;
   let userID = req.cookies['user_id'];
-  let templateVars = {userURLs:userURLs,user_id:userID}
-  console.log("Show me urls!", userURLs)
+  let templateVars = {userURLs: userURLs,
+                      user_id: userID}
   res.render('urls_home', templateVars);
 });
 
@@ -145,12 +126,17 @@ app.get("/login", (req, res) => {
 /***********************************Post Requests*******************************/
 
 app.post("/urls", (req, res) => {
-  let url = {};
-  url.shortURL = randomURL;
+  let url = {}
+  let userid = req.cookies['user_id']
+  url.shortURL = generateRandomString();
   url.longURL = req.body['longURL'];
-  urlDatabase[uid] = url;
+  if (!urlDatabase[userid]){ //value
+     urlDatabase[userid] = [url] //value = something
+   } else {urlDatabase[userid].push(url);
+  //add something to value
+  }
   console.log(urlDatabase, "This should be the url dict");
-  res.redirect("/urls");
+  res.redirect("/");
 });
 
 app.post("/register", (req, res) => {
@@ -167,14 +153,14 @@ app.post("/urls/:id/delete", (req, res) => {
     user_id: req.cookies["user_id"]
   };
   delete urlDatabase[templateVars.shortURL];
-  res.redirect("/urls");
+  res.redirect("/");
 });
 
 app.post('/urls/:id/update', (req, res) => {
   if(urlDatabase[req.params.id]) {
     urlDatabase[req.params.id] = req.body.longURL;
   }
-  res.redirect('/urls')
+  res.redirect('/')
 });
 
 app.post("/login", (req, res) => {
